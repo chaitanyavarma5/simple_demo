cb98b3a18c6f117526f7a204e7a4a3fa22eb9b8d
chaitanyavarma5 <chaitanya.varma5@gmail.com>
2024-04-25 15:38:10 +0530

    updated with test scenarios

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,3 +2,27 @@ def test_generic() :
     a = 2
     b = 2
     assert a == b
+
+
+def test_generic1() :
+    a = 2
+    b = 2
+    assert a != b
+
+
+class NotInRange(Exception) :
+    def __init__(self, message="value not in given range- by BVR") :
+        self.message = message
+        super().__init__(self.message)
+
+
+def test_generic2() :
+    a = 15
+    if a not in range(10, 20) :
+        raise NotInRange
+    
+
+def test_generic3() :
+    a = 5
+    if a not in range(10, 20) :
+        raise NotInRange
